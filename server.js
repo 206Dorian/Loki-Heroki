8f78cbd4a7290bfd7bdc49759f05794f5d447659
Dorian Birch <206dorian@gmail.com>
2022-11-10 17:14:07 -0600

    debugged notes, running in dev, but not start

--- server.js
+++ server.js
@@ -1,37 +1,52 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
-
+const clog = require('clog');
+// const api = require('./routes/index.js');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-
+const db = require('./db/db.json');
 // Import custom middleware, "cLog"
-app.use(clog);
+// app.use(clog);
+const uuid = require('./helper/uuid');
+const fs = require('fs');
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/api/notes', (req, res) => {
+  res.json(db)
+})
+
+app.post('/api/notes', (req, res) => {
+  const newNote = db
+  console.log(newNote)
+  const noteContent = { title: req.body.title, text: req.body.text, id: uuid() }
+  newNote.push(noteContent)
+  console.log(noteContent)
+
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(newNote))
+  res.json(newNote)
+})
+
+
+// GET Route for notes
+app.get('/notes', (_req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+// // GET Route for feedback page
+// app.get('/feedback', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+// );
 
-// Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-);
-
+// // Wildcard route to direct users to a 404 page
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+// );
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
